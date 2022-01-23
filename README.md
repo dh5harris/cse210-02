db14383f249e87c5ada74a35c532b9ddeaab0cd5
Your Name <Weylin76@msn.com>
2022-01-23 10:18:37 -0500

    added functions

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cse210-02
 # Team project file for CSE 210
 #This is Weylin
+  
--- code/hilo_game.py
+++ code/hilo_game.py
@@ -1,30 +1,41 @@
 import random
+def main():
+    points = 300
+    play_game(points)
 
-points=300
+def play_game(points):
+    while points >0:
+        card_value_initial = random.randint(1,13)
+        print (f"Dealer's new card is: {card_value_initial}")
+        player_guess = input(("Do you believe the card will be higher or lower (H/L): "))
+        card_value_next = random.randint(1,13) 
+        print(card_value_next)
+        score_adjustment = scoring(player_guess,card_value_initial,card_value_next)
+        points += score_adjustment
+        print(points)
+        countinue_playing = input("Do you wish to contine? Y/N: ")
+        if countinue_playing.upper() == "Y":
+            continue
+        else:
+            break
 
-countinue_playing = "Y"
-
-#def continue_playing (points):
-while points >0 and countinue_playing.upper() == "Y":  #need to add if you still want to play
-    print()
-    card_value_initial = random.randint(1,13)
-    print (f"Dealer's new card is: {card_value_initial}")
-    player_guess = input(("Do you believe the card will be higher or lower (H/L): "))
-    card_value_next = random.randint(1,13)
+def scoring(player_guess,card_value_initial,card_value_next):
     if player_guess.upper() == "H"  and card_value_next > card_value_initial:
-        points = points + 100
+        score_adjustment = 100
         print (f"Dealers card is: {card_value_next}. You were correct You earned 100 points!")
     elif player_guess.upper() == "H"  and card_value_next <= card_value_initial:
-        points = points - 75
+        score_adjustment = -75
         print(f"Dealers card is: {card_value_next}. Sorry, you were incorrect.  You lost 75 points.")
     elif player_guess.upper() == "L"  and card_value_next < card_value_initial:
-        points = points + 100
+        score_adjustment = 100
         print(f"Dealers card is: {card_value_next}. You were correct!  You earned 100 points")
     elif player_guess.upper() == "L"  and card_value_next >= card_value_initial:
-        points = points - 75
+        score_adjustment = -75
         print (f"Dealers card is: {card_value_next}. Sorry you were incorrect. You lost 75 points")
-    print (points)
-    countinue_playing = input("Do you wish to contine? Y/N: ")
+    return score_adjustment
+    
+if __file__==main():
+    main()
 
 
 
